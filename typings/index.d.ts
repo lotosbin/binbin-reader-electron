50b2ab0d11de7329ee71aebf691039c1bdd35b05
刘斌斌 <liubinbin@liubinbindeMac-mini.local>
2016-08-21 22:46:16 +0800

    git rm -r typings;git ignore typings/

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/github-electron/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/vue/index.d.ts" />
