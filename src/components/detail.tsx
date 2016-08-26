9edaf377f06cf490041e346e3e1ee3d9f630e6b4
liubinbin <lotosbin@gmail.com>
2016-08-26 17:26:38 +0800

    fix

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -1,61 +1,73 @@
 /**
  * Created by liubinbin on 16/08/2016.
  */
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import RaisedButton from 'material-ui/RaisedButton';
-import LinearProgress from 'material-ui/LinearProgress';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from "material-ui/Toolbar";
+import RaisedButton from "material-ui/RaisedButton";
+import LinearProgress from "material-ui/LinearProgress";
 import {IArticle} from "../../definitions/storage/article";
-import * as React from 'react'
-import {shell} from 'electron'
+import * as React from "react";
+import {shell} from "electron";
 import emitter from "./emitter";
-import articleStorage from '../storage/article'
+import articleStorage from "../storage/article";
 import {doSegment} from "../functions/segment";
 
 
 export interface DetailWebViewProps {
 
 }
-export class DetailWebView extends React.Component<DetailWebViewProps,{}> {
+export class DetailWebView extends React.Component<DetailWebViewProps, {}> {
   state = {
-    url: "http://www.yuanjingtech.com"
-  }
+    url: "http://www.yuanjingtech.com",
+  };
 
-  componentWillMount() {
+  public componentWillMount() {
     emitter.on("open_in_detail", (entry: IArticle) => {
-      console.log('bind webview events')
-      var detialwebview = document.getElementById('webview');
-      detialwebview.addEventListener('did-start-loading', ()=> {
-        console.log('did-start-loading')
-        emitter.emit('detail:did-start-loading')
-      })
-      detialwebview.addEventListener('did-stop-loading', () => {
-        console.log('did-stop-loading')
+      console.log("bind webview events");
+      // let detialwebview = document.getElementById("webview");
+      // detialwebview.addEventListener("did-start-loading", () => {
+      //   console.log("did-start-loading");
+      //   emitter.emit("detail:did-start-loading");
+      // });
+      // detialwebview.addEventListener("did-stop-loading", () => {
+      //   console.log("did-stop-loading");
 
-        articleStorage.Read({id: this.state.url}, ()=> {
-        })
-        emitter.emit('detail:did-stop-loading')
-        emitter.emit('refresh_list', {})
-      });
+      //   articleStorage.Read({id: this.state.url}, () => {
+      //   });
+      //   emitter.emit("detail:did-stop-loading");
+      //   emitter.emit("refresh_list", {});
+      // });
       this.setState({
-        url: entry.link
-      })
-      articleStorage.Read({id: this.state.url}, ()=> {
-      })
+        url: entry.link,
+      });
+      articleStorage.Read({id: this.state.url}, () => {
+      });
     });
   }
 
-  componentDidMount() {
+  public componentDidMount() {
+      let detialwebview = document.getElementById("webview");
+      detialwebview.addEventListener("did-start-loading", () => {
+        console.log("did-start-loading");
+        emitter.emit("detail:did-start-loading");
+      });
+      detialwebview.addEventListener("did-stop-loading", () => {
+        console.log("did-stop-loading");
 
+        articleStorage.Read({id: this.state.url}, () => {
+        });
+        emitter.emit("detail:did-stop-loading");
+        emitter.emit("refresh_list", {});
+      });
   }
 
-  styles = {
+  public styles = {
     webview: {
-      display: 'flex',
-      flex: 1
-    }
-  }
+      display: "flex",
+      flex: 1,
+    },
+  };
 
-  render() {
+  public render() {
     return (
       <webview id="webview"
                style={this.styles.webview}
@@ -65,10 +77,10 @@ export class DetailWebView extends React.Component<DetailWebViewProps,{}> {
     );
   }
 }
-export interface DetailProps {
+export interface IDetailProps {
 
 }
-export class Detail extends React.Component<DetailProps,{}> {
+export class Detail extends React.Component<IDetailProps, {}> {
   public Init() {
 
   }
@@ -78,26 +90,26 @@ export class Detail extends React.Component<DetailProps,{}> {
 export interface DetailToolBarProps {
 
 }
-export class DetailToolBar extends React.Component<DetailToolBarProps,{}> {
+export class DetailToolBar extends React.Component<DetailToolBarProps, {}> {
   state = {
     url: "",
-    title: ""
-  }
+    title: "",
+  };
 
   componentWillMount() {
-    emitter.on('open_in_detail', (entry: IArticle)=> {
-      var segments: string[] = doSegment(entry.title);
-      console.log('segments:' + JSON.stringify(segments))
+    emitter.on("open_in_detail", (entry: IArticle) => {
+      let segments: string[] = doSegment(entry.title);
+      console.log("segments:" + JSON.stringify(segments));
       this.setState({
         url: entry.link,
         title: entry.title,
-        segments: segments
-      })
-    })
+        segments: segments,
+      });
+    });
   }
 
   onOpenInBrowser() {
-    shell.openExternal(this.state.url)
+    shell.openExternal(this.state.url);
   }
 
   render() {
@@ -117,22 +129,22 @@ export class DetailToolBar extends React.Component<DetailToolBarProps,{}> {
 export interface DetailProgressProps {
 
 }
-export class DetailProgress extends React.Component<DetailProgressProps,{}> {
+export class DetailProgress extends React.Component<DetailProgressProps, {}> {
   state = {
-    value: 0
-  }
+    value: 0,
+  };
 
   componentWillMount() {
-    emitter.on('detail:did-start-loading', ()=> {
+    emitter.on("detail:did-start-loading", () => {
       this.setState({
-        value: 20
-      })
-    })
-    emitter.on('detail:did-stop-loading', ()=> {
+        value: 20,
+      });
+    });
+    emitter.on("detail:did-stop-loading", () => {
       this.setState({
-        value: 100
-      })
-    })
+        value: 100,
+      });
+    });
   }
 
   render() {
@@ -144,5 +156,5 @@ export class DetailProgress extends React.Component<DetailProgressProps,{}> {
   }
 }
 
-var detail = new Detail();
-export default detail
+let detail = new Detail();
+export default detail;
--- src/storage/article.ts
+++ src/storage/article.ts
@@ -1,66 +1,66 @@
 /**
  * Created by liubinbin on 15/08/2016.
  */
-import * as _ from 'lodash'
-const ArticleTableName = "ARTICLES3"
+import * as _ from "lodash";
+const ArticleTableName = "ARTICLES3";
 class ArticleStorage {
   Add({title, link, feed_xmlurl}, callback) {
-    var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
-    var msg;
+    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
+    let msg;
     db.transaction(function (tx) {
       tx.executeSql(`INSERT INTO ${ArticleTableName}  (id, title,link,feed_xmlurl,readed) VALUES (? ,?,?,?,?)`, [link, title, link, feed_xmlurl, 0], function (transaction, results) {
 
       }, function (transation, error) {
-        console.log(error)
+        console.log(error);
       });
     });
   }
 
   Read({id}, callback) {
-    console.log('read:' + id)
-    var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
-    var msg;
+    console.log("read:" + id);
+    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
+    let msg;
     db.transaction(function (tx) {
       tx.executeSql(`UPDATE ${ArticleTableName} SET readed = 1 WHERE id=? `, [id], function (transaction, results) {
 
       }, function (transation, error) {
-        console.log(error)
+        console.log(error);
       });
     });
   }
 
   Init() {
-    var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
-    var msg;
+    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
+    let msg;
 
     db.transaction(function (tx) {
       tx.executeSql(`CREATE TABLE IF NOT EXISTS ${ArticleTableName} (id UNIQUE, title, link, feed_xmlurl,readed)`);
-      msg = '<p>created </p>';
-      console.log(msg)
+      msg = "<p>created </p>";
+      console.log(msg);
     });
   }
 
 
   AddRange(items, feed_xmlurl, callback) {
-    _(items).forEach((value)=> {
-      this.Add({title: value.title, link: value.link, feed_xmlurl: feed_xmlurl}, ()=> {
-      })
-    })
+    _(items).forEach((value) => {
+      this.Add({title: value.title, link: value.link, feed_xmlurl: feed_xmlurl}, () => {
+      });
+    });
   }
 
   Find({}, callback) {
-    var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
-    var msg;
+    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
+    let msg;
 
     db.transaction(function (tx) {
       tx.executeSql(`SELECT * FROM ${ArticleTableName}  ORDER BY readed,rowid DESC LIMIT 100`, [], function (tx, results) {
-        callback(null, results)
+        callback(null, results);
       }, null);
     });
   }
 }
 
-var articleStorage = new ArticleStorage();
-articleStorage.Init()
+let articleStorage = new ArticleStorage();
+articleStorage.Init();
 
-export default articleStorage
+export default articleStorage;
