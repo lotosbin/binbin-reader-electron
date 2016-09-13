177d14e598c73e09e49b9a0fd9f00e9b1d98dd15
liubinbin <lotosbin@gmail.com>
2016-09-13 13:30:12 +0800

    all use react

--- src/index.tsx
+++ src/index.tsx
@@ -12,31 +12,30 @@ injectTapEventPlugin();
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {SideBar} from "./components/sidebar";
 
-ReactDOM.render(
-  <MuiThemeProvider>
-    <TopBar />
-  </MuiThemeProvider>
-  , document.getElementById("topbar"));
-// var bottomNavigation = <div>
-//   <a href="https://github.com/lotosbin/binbin-reader-electron" target="_blank">GitHub</a>
-// </div>;
-var bottomNavigation =
-  <MuiThemeProvider>
-    <BottomNavigation></BottomNavigation>
-  </MuiThemeProvider>;
-ReactDOM.render(bottomNavigation, document.getElementById("status"));
 var detail = (
   <MuiThemeProvider>
-    <Detail></Detail>
+    <div style={{display:'flex',flexDirection:'column'}}>
+      <div id="topbar" className="" style={{display: 'flex',flexDirection: 'row', flex:'none'}}>
+        <TopBar />
+      </div>
+      <div className="content" style={{display:'flex'}}>
+        <div id="divsideBar" className="column" style={{flexGrow: 0,flexBasis: '20%'}}>
+          <SideBar></SideBar>
+        </div>
+        <div id="detail" className="column" style={{flex:1}}>
+          <Detail></Detail>
+        </div>
+      </div>
+      <div className="footer" id="status" name="status">
+        <BottomNavigation></BottomNavigation>
+        <div className="column" id="app" style={{display:'flex'}}>
+          <div className="row" style={{justifyContent: 'flex-end'}}>
+            <button style={{flex:'none'}} onClick="onImport()">import opml</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </MuiThemeProvider>
 );
-
-ReactDOM.render(detail, document.getElementById("detail"));
-var sideBar = (
-  <MuiThemeProvider>
-    <SideBar></SideBar>
-  </MuiThemeProvider>
-
-);
-ReactDOM.render(sideBar, document.getElementById("divsideBar"));
+ReactDOM.render(detail, document.getElementById("body"));
 
