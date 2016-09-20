a7dcca866bb63bcba038ac61348c1a5ea23515bf
刘斌斌 <liubinbin@liubinbindeMac-mini.local>
2016-09-20 22:50:58 +0800

    .

--- src/storage/history.ts
+++ src/storage/history.ts
@@ -4,6 +4,8 @@
 import * as _ from "lodash";
 import values = require("lodash/values");
 import {IArticle} from "../../definitions/storage/article";
+import {Storage} from "./storagebase"
+
 export interface ISuccessCallback<TData> {
   (error: any, data: TData): void
 }
@@ -15,41 +17,10 @@ export class History implements IHistory {
   id: number;
   articleId: string;
 }
-export abstract class Storage<M> {
-  model: M;
-
-  abstract  tableName(): void
-
-  abstract  columnList(): void
-
-  Add(model: IHistory, callback: ISuccessCallback<any>) {
-    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
-    db.transaction((tx)=> {
-      tx.executeSql(`INSERT INTO ${this.tableName()}  (${this.columnList()}) VALUES (?)`, this.columnValues(model), (transaction, results)=> {
-        if (callback)callback(null, null);
-      }, (transation, error) => {
-        console.log(error);
-      });
-    });
-  }
-
-  AddAsync(model: IHistory) {
-    return new Promise((resolve, reject)=> {
-      this.Add(model, ()=> {
-        resolve(model)
-      })
-    })
-  }
-
-  Init() {
-    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
-    db.transaction((tx) => {
-      tx.executeSql(`CREATE TABLE IF NOT EXISTS ${this.tableName()} (id UNIQUE, ${this.columnList()})`);
-      console.log("<p>created </p>");
-    });
-  }
-}
 class HistoryStorage extends Storage<History> {
+  Find({}:{}, callback: ISuccessCallback<History[]>): void {
+  }
+
   tableName() {
     return "HISTORY"
   }
@@ -64,16 +35,40 @@ class HistoryStorage extends Storage<History> {
     return values;
   }
 
-  AddRange(items, feed_xmlurl, callback) {
+  Add(model: IHistory, callback: ISuccessCallback<any>) {
+    this.open().transaction((tx) => {
+      tx.executeSql(`INSERT INTO ${this.tableName()}  (${this.columnList()}) VALUES (?)`, this.columnValues(model), (transaction, results) => {
+        if (callback)callback(null, null);
+      }, (transation, error) => {
+        console.log(error);
+      });
+    });
+  }
+
+  AddAsync(model: IHistory) {
+    return new Promise((resolve, reject) => {
+      this.Add(model, () => {
+        resolve(model)
+      })
+    })
+  }
+
+  Init() {
+    this.open().transaction((tx) => {
+      tx.executeSql(`CREATE TABLE IF NOT EXISTS ${this.tableName()} (id UNIQUE, ${this.columnList()})`);
+      console.log("<p>created </p>");
+    });
+  }
+
+  AddRange(items: any[], feed_xmlurl: string, callback: any) {
     _(items).forEach((value) => {
       this.Add({title: value.title, link: value.link, feed_xmlurl: feed_xmlurl}, () => {
       });
     });
   }
 
-  Find({}, callback: ISuccessCallback<IArticle[]>) {
-    let db = openDatabase("mydb", "1.0", "Test DB", 2 * 1024 * 1024);
-    db.transaction((tx) => {
+  FindArticle({}, callback: ISuccessCallback<IArticle[]>) {
+    this.open().transaction((tx) => {
       tx.executeSql(`SELECT h.id AS hId,a.* FROM  ${this.tableName()} h LEFT JOIN ARTICLE3 a ON h.articleId=a.id ORDER BY rowid DESC LIMIT 100`, [], (tx, results) => {
         var d: IArticle[] = [];
         for (var i = 0; i < results.rows.length; i++) {
@@ -85,8 +80,8 @@ class HistoryStorage extends Storage<History> {
   }
 
   FindAsync() {
-    return new Promise((resolve, reject)=> {
-      this.Find({}, (error: any, data: IArticle[])=> {
+    return new Promise((resolve, reject) => {
+      this.FindArticle({}, (error: any, data: IArticle[]) => {
         resolve(data)
       })
     })
